0510edc729c9df99a65ff63349e7e5c65f4cb999
Mahesh <33093725+MaheshMote12@users.noreply.github.com>
2019-09-28 10:24:57 -0700

    initial commit

---
+++ zuul-gateway-with-ribbon/src/main/java/com/hurix/HystrixFallbackConfiguration.java
@@ -0,0 +1,17 @@
+//package com.hurix;
+//
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//
+//@Configuration
+//public class HystrixFallbackConfiguration {
+//	
+//	@Bean
+//	public ZuulFallbackProvider zuulFallbackProvider() {
+//		
+//		return new ZuulFallbackProvider() {
+//			 
+//		}
+//	}
+//
+//}
